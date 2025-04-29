7144b5bfca9fac2da658424494a24bf38806184d
AllyssaJohnston <168232004+AllyssaJohnston@users.noreply.github.com>
2025-04-29 11:41:37 -0700

    Added in targeting for enemy spells, and autocreation of character stat and icon ui

--- DungeonCrawler/Assets/Scripts/Combat/CharacterStatBehavior.cs
+++ DungeonCrawler/Assets/Scripts/Combat/CharacterStatBehavior.cs
@@ -3,19 +3,12 @@ using UnityEngine;
 
 public class CharacterStatBehavior : MonoBehaviour
 {
-    public CharacterBehavior character;
     public TMP_Text healthText;
     public TMP_Text moraleText;
 
-    private void Start()
+    public void updateText(int health, int morale)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        healthText.text = character.getHealth().ToString();
-        moraleText.text = character.getMorale().ToString();
+        healthText.text = health.ToString();
+        moraleText.text = morale.ToString();
     }
 }
