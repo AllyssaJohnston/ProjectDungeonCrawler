8c5490600d80e6ea4913a2091c937fcf8434050d
AllyssaJohnston <168232004+AllyssaJohnston@users.noreply.github.com>
2025-05-05 22:51:50 -0700

    Modified enemy turn arrows

--- DungeonCrawler/Assets/Scripts/Combat/ArrowIndicatorBehavior.cs
+++ DungeonCrawler/Assets/Scripts/Combat/ArrowIndicatorBehavior.cs
@@ -2,26 +2,34 @@ using UnityEngine;
 
 public class ArrowIndicatorBehavior : MonoBehaviour
 {
+    bool move = true;
     Vector3 startPos;
     Vector3 movementVect;
     float bufferTimer = 0f;
     [SerializeField] float movementTime;
 
-    public void setUp(float rot)
+    public void setUp(float rot, bool move)
     {
         startPos = transform.localPosition;
         float rad = (rot * Mathf.PI) / 180f;
         movementVect = 60 * new Vector3(Mathf.Cos(rad), Mathf.Sin(rad));
+        this.move = move;
     }
 
     private void Update()
     {
-        buffer();
+        if (move)
+        {
+            buffer();
+        }
     }
 
     private void FixedUpdate()
     {
-        transform.localPosition += movementVect * Time.fixedDeltaTime;
+        if (move)
+        {
+            transform.localPosition += movementVect * Time.fixedDeltaTime; ;
+        }
     }
 
     private void buffer()
